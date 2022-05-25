63b743d66561eeae45d7f108b443db2954ab779e
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-05-26 00:51:30 +0545

    created api for create user and get user along with jwt

--- src/conroller/userController.js
+++ src/conroller/userController.js
@@ -1,29 +1,49 @@
 //
-import Blog from "../models/userModel.js";
 import jsonwebtoken from "jsonwebtoken";
-import { json } from "express";
+import UserModel from "../models/userModel.js";
+import AddressModel from "../models/addressModel.js";
 class UserController {
   async createUser(req, res) {
-    const blog = new Blog({ title: "THis is title", author: "Author name" });
-    const token = jsonwebtoken.sign({ title: blog.title }, "hhh");
-    blog.token = token;
-    console.log(token);
-    try {
-      // const result = await blog.save(function (err, data) {
-      //   if (err) {
-      //     console.log(err);
-      //   } else {
-      //     res.send({ messsage: "Data inserted", data: data });
-      //   }
-      // });
-      // if (result) {
-      //   console.log(`result == ${result}`);
-      // }
+    const { name, email, password, image_url, phone } = req.body;
+    const address = req.body.address;
+    const addressModel = new AddressModel(address);
 
-      const result = jsonwebtoken.verify(token, "hhh");
-      res.send(result);
+    const token = jsonwebtoken.sign({ email, phone }, "clickmind");
+
+    try {
+      const user = new UserModel({
+        email: email,
+        name: name,
+        password: password,
+        image_url: image_url,
+        phone: phone,
+        address: (await addressModel.save())._id,
+        token: token,
+      });
+      const result = await user.save(function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send({ messsage: "Data inserted", data: data });
+        }
+      });
+      if (result) {
+        console.log(`result == ${result}`);
+      }
     } catch (e) {
-      res.send({ error: e });
+      res.send({ error: `${e}` });
+    }
+  }
+  async getUser(req, res) {
+    try {
+      const token = req.headers.authorization;
+      const email = jsonwebtoken.verify(token, "clickmind");
+      const user = await UserModel.findOne(email, "-__v -token -_id");
+      const address = await AddressModel.findOne(user.address._id, "-__v");
+      user.address = address;
+      res.send({ success: true, data: user });
+    } catch (e) {
+      res.send({ message: `${e}` });
     }
   }
 }
