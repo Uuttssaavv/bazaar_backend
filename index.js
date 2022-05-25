47ea74c5022060662046ed307b7d8bdc8acc2fe3
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-05-25 15:34:03 +0545

    integrated mongoosw

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyPraser from "body-parser";
+import connectToServer from "./src/models/server.js";
+import UserController from "./src/conroller/userController.js";
+const app = express();
+const router = express.Router();
+const port = 5000;
+app.use(bodyPraser.json());
+const uc = new UserController();
+app.use("/user/create", uc.createUser);
+
+app.listen(port, async () => {
+  try {
+    var res = await connectToServer();
+    console.log(res);
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  console.log("Listening to port:  " + port);
+});
---
+++ src/models/server.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const mongoAtlasUri =
+  "mongodb+srv://utsav:passwordtest@cluster0.x9hdx.mongodb.net/?retryWrites=true&w=majority";
+//
+const connectToServer = async () => {
+  try {
+    // Connect to the MongoDB cluster
+    await mongoose.connect(
+      mongoAtlasUri,
+
+      () => console.log(" Mongoose is connected")
+    );
+  } catch (e) {
+    console.log("could not connect");
+  }
+};
+export default connectToServer;
