911d33a9a0d76d64ea3a91cf9bb5ad0cabcc486e
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-05-29 16:07:58 +0545

    user model till login

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import AddressModel from "./addressModel.js";
 
 const UserSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
@@ -6,36 +7,48 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Email can't be blank"],
     match: [/\S+@\S+\.\S+/, "Invalid email "],
-    unique: true,
+    unique: [true, "Email already in use."],
   },
   name: String,
   password: {
     type: String,
+    minlength: [8, "Password cannot be less than 8 characters"],
     required: [true, "Password can't be blank"],
     select: false,
   },
   token: {
     type: String,
     required: [true, "Token can't be blank"],
-    unique: true,
+
+    unique: [true, "Token already in use."],
   },
   resresh_token: String,
   image_url: String,
   phone: {
     type: String,
+    validate: {
+      validator: function (val) {
+        return val.toString().length === 10;
+      },
+      message: "Invalid phone number.",
+    },
     required: [true, "Phone number can't be blank"],
-    unique: true,
+    unique: [true, "Phone already in use."],
   },
   address: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "address",
   },
 });
-UserSchema.methods.validPassword = function (password) {
-  var hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
-  return this.hash === hash;
+UserSchema.methods.validPassword = async (password) => {
+  const user = await UserModel.findOne({ password: password });
+
+  return user != null;
+};
+UserSchema.methods.getAddress = async (user) => {
+  const address = await AddressModel.findOne({ _id: user.address._id }, "-__v");
+  user.address = address;
+  return user;
 };
 
 const UserModel = mongoose.model("User", UserSchema);
