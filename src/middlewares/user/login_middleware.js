c715696bf429f64e0a30bcb101d2ecb1fcae2ffe
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-07-07 15:20:59 +0545

    optimized login middleware

--- src/middlewares/user/login_middleware.js
+++ src/middlewares/user/login_middleware.js
@@ -21,18 +21,29 @@ const generateValidators = () => [
       .withMessage("Phone number invalid"),
   ]),
   check("password", "Password cannot be empty").notEmpty(),
+  check("new_password", "Please enter new password").notEmpty(),
 ];
 
 const reporter = async (req, res, next) => {
-  const { email, phone } = req.body;
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    const errorMessages = errors
-      .array()
-      .map((error) =>
-        error.nestedErrors ? error.nestedErrors[0].msg : error.msg
-      );
+  var errors = validationResult(req);
+  const index = errors.array().findIndex((e) => e.param === "new_password");
+  if (req.path === "/login") {
+    errors = remove(errors.array(), errors.array()[index]);
+  } else {
+    errors = errors.array();
+  }
+  console.log(index);
+  console.log(req.path);
+  console.log(errors);
+  if (errors.length !== 0) {
+    const errorMessages = errors.map((error) => {
+      return error.nestedErrors
+        ? {
+            key: error.nestedErrors[0].param,
+            message: error.nestedErrors[0].msg,
+          }
+        : { key: error.param, message: error.msg };
+    });
 
     return res.status(400).json({
       success: false,
@@ -40,9 +51,14 @@ const reporter = async (req, res, next) => {
       message: "Validation error",
     });
   } else {
+    console.log("next");
     next();
   }
 };
-
+function remove(arrOriginal, elementToRemove) {
+  return arrOriginal.filter(function (el) {
+    return el !== elementToRemove;
+  });
+}
 const loginMiddleware = [generateValidators(), reporter];
 export default loginMiddleware;
