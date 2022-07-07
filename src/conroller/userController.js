8f9be241105be328c7f17d9c3f5ee2bce1648d31
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-07-07 15:40:06 +0545

    completed update password

--- src/conroller/userController.js
+++ src/conroller/userController.js
@@ -6,7 +6,6 @@ import bcrypt from "bcryptjs";
 class UserController {
   async register(req, res) {
     const { name, email, password, image_url, phone, address } = req.body;
-    const addressModel = new AddressModel(address);
     const encrpassword = await bcrypt.hash(password, 8);
 
     try {
@@ -17,7 +16,11 @@ class UserController {
         image_url: image_url,
         phone: phone,
       });
-      user.address = await addressModel.save();
+      if (address) {
+        const addressModel = new AddressModel(address);
+
+        user.address = await addressModel.save();
+      }
       const usr = await user.save();
       const token = jsonwebtoken.sign({ _id: usr._id }, "clickmind");
       usr.token = token;
@@ -28,6 +31,36 @@ class UserController {
       }
     } catch (e) {
       res.send({ success: false, message: "Error" + `${e}` });
+    }
+  }
+  async changePassword(req, res) {
+    const { email, password, new_password, user } = req.body;
+    try {
+      const encrpassword = await bcrypt.hash(new_password, 8);
+      console.log(user);
+      const usr = await UserModel.findOne({ _id: user._id }, "+password");
+      console.log(usr.password);
+      console.log(password);
+      const isPasswordValid = bcrypt.compareSync(password, usr.password);
+      if (isPasswordValid) {
+        await UserModel.findByIdAndUpdate(user.id, { password: encrpassword });
+        res
+          .status(201)
+          .json({
+            success: true,
+            message: "Changed password successfully",
+            user: user,
+          });
+      } else {
+        res.status(401).send({ success: false, message: "Incorrect password" });
+      }
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: "Unable to change password",
+
+        error: `Error ${error}`,
+      });
     }
   }
 
@@ -49,10 +82,7 @@ class UserController {
         const token = jsonwebtoken.sign({ _id: user._id }, "clickmind");
         user.token = token;
         console.log(token);
-        const passwordIsValid = bcrypt.compareSync(
-          req.body.password,
-          user.password
-        );
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
         if (passwordIsValid) {
           delete user.password;
           res.status(200).json({ success: true, data: user });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,18 @@ import express from "express";
 import UserController from "../conroller/userController.js";
 import signupMiddleware from "../middlewares/user/signup_middleware.js";
 import loginMiddleware from "../middlewares/user/login_middleware.js";
-// import authenticationMiddleware from "../middlewares/authentication_middleware.js";
+import authenticationMiddleware from "../middlewares/authentication_middleware.js";
 const router = express.Router();
 const userController = new UserController();
 router.post("/login", loginMiddleware, userController.login);
 
 router.post("/create", signupMiddleware, userController.register);
+
+router.post(
+  "/updatePassword",
+  loginMiddleware,
+  authenticationMiddleware,
+  userController.changePassword
+);
 
 export default router;
